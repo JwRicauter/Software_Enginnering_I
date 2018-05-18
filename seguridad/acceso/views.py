ad9b3a841228fd19d477207fb63a81b8d16e9fb9
JwRicauter <ricauterjawil@gmail.com>
2018-05-17 20:43:54 -0400

    Segundo avance

--- seguridad/acceso/views.py
+++ seguridad/acceso/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from acceso.models import Usuario
 
+
 # Create your views here.
 
 def sesion(request):
-	print(request.method)
-	if request.method == 'GET':
-		print("askjdn")
-		status = request.GET.get('correoUser')
-		print(status)
+
+
+
+	if request.method == 'POST':
+		indicador = request.POST['indicador']
+		if (indicador == 'login'):
+			loginUser = request.POST['loginUser']
+			loginPwd = request.POST['loginPwd']
+		elif (indicador == 'registro'):
+			registroUser = request.POST['registroUser']
+			registroPwd1 = request.POST['registroPwd1']
+			registroPwd2 = request.POST['registroPwd2']
+		
 
 	return render(request, 'acceso/sesion.html')	
