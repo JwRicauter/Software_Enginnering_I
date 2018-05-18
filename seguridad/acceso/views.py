d876aa8aebf00e44a559e16c60b8ca246b83bbd0
David Cabeza <cabezadavide@gmail.com>
2018-05-17 22:04:16 -0400

    Se muestran errores de usuario invalido al login. Los errores de registro estan en prueba

--- seguridad/acceso/views.py
+++ seguridad/acceso/views.py
@@ -1,23 +1,45 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from acceso.models import Usuario
+from .seguridad import Seguridad	
 
-
-# Create your views here.
+seguridad = Seguridad()
 
 def sesion(request):
 
+	context = {}
 
+	context['error'] = False
 
 	if request.method == 'POST':
 		indicador = request.POST['indicador']
 		if (indicador == 'login'):
 			loginUser = request.POST['loginUser']
 			loginPwd = request.POST['loginPwd']
+			info = seguridad.ingresarUsuario(loginUser, loginPwd)
+
+			# Verificar si el usuario aprobó los test de seguridad
+			if info == True:
+				context['success'] = "Conectado"
+				return render(request, 'acceso/ok.html', context)
+			else:
+				context['error'] = True
+				context['error_msg'] = info
+				return render(request, 'acceso/sesion.html', context)
+		
 		elif (indicador == 'registro'):
 			registroUser = request.POST['registroUser']
 			registroPwd1 = request.POST['registroPwd1']
 			registroPwd2 = request.POST['registroPwd2']
-		
 
-	return render(request, 'acceso/sesion.html')	
+			info = seguridad.registrarUsuario(registroUser, registroPwd1, registroPwd2)
+
+			if info == True:
+				context['success'] = "Registrado"
+				return render(request, 'acceso/ok.html', context)
+			else:
+				context['error'] = True
+				context['error_msg_registro'] = info
+				return render(request, 'acceso/sesion.html', context)
+
+	return render(request, 'acceso/sesion.html', context)	
